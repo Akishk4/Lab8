ce24c501addc3afcd9d39f3565f34f13ca0d94aa
Akishk4 <kishkabdelrahman951@gmail.com>
2025-11-23 18:31:40 +0200

    Create Course.java

---
+++ src/BackEnd/Course.java
@@ -0,0 +1,58 @@
+package BackEnd;
+
+import java.util.*;
+
+public class Course {
+
+    String courseId;
+    String title;
+    String description;
+    String instructorId;
+    String status;
+
+    List<Lesson> lessons = new ArrayList<>();
+    List<String> studentIds = new ArrayList<>();
+
+    public Course() {
+    }
+
+    public Course(String title, String description, String instructorId) {
+        this.courseId = UUID.randomUUID().toString();
+        this.title = title;
+        this.description = description;
+        this.instructorId = instructorId;
+        this.status = "PENDING";
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String s) {
+        this.status = s;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public List<String> getStudentIds() {
+        return studentIds;
+    }
+}
